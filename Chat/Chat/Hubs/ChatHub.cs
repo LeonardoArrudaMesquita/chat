163f08971e2a0e14ab4bc2bf2240a3e3ec22e0ea
LeonardoArrudaMesquita <leonardoarruda@outlook.com.br>
2019-09-08 15:41:54 -0300

    Chat Project

---
+++ Chat/Chat/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Hubs
+{
+    // The Hub class manages connections, groups, and messaging.
+    public class ChatHub : Hub
+    {
+        // Async para ter mais escablidade (Preparado pra crescer)
+        public async Task EnviarMensagem(string usuario, string mensagem)
+        {
+            // await -> operador que suspende o metodo async até que que seja completada
+            await Clients.All.SendAsync("ReceberMensagem", usuario, mensagem);
+        }
+
+    }
+}
